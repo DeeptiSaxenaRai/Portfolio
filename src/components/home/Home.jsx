c8d9228b88337787ba25a8e4a1d5dd28e6d6513f
DeeptiSaxenaRai <deepti.kodehode@gmail.com>
2023-05-08 23:04:44 +0200

    initial commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,13 @@
 import "./home.css";
+import arrow from "../../assets/image/down-arrow-icon.png";
+
 function Home() {
   return (
     <div className="homeContainer" id="home">
       <h2 className="heading">Hi I'm</h2>
       <h1 className="mainHeading">DEEPTI SAXENA</h1>
       <h2 className="heading">Frontend Developer</h2>
+      <img src={arrow} />
     </div>
   );
 }
